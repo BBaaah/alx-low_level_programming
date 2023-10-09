9ff669e126f0160df26bb5047798ae08e87e87a0
Bbaaah <Bbaaah>
2023-10-08 23:03:40 -0700

    update

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,7 +8,7 @@
  */
 int main(void)
 {
-    const char *str = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-    write(STDOUT_FILENO, str, 59); /* Using write to print to stdout */
-    return (1);
+	const char *str = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	write(STDOUT_FILENO, str, 59); /* Using write to print to stdout */
+	return (1);
 }
