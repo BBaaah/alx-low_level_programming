6be166c26098aaf78653a81f17067a44321744bb
Bbaaah <Bbaaah>
2023-10-08 23:08:53 -0700

    update

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,11 +4,12 @@
  * main - A c program that prints a line to the standard error
  * The entry point of the program
  * This function writes a specific string to the standard output (stdout)
- * Return 1 (Success)
+ * @return Always returns 1 to indicate an error occurred.
  */
 int main(void)
 {
 	const char *str = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	write(STDOUT_FILENO, str, 59); /* Using write to print to stdout */
+	write(STDOUT_FILENO, str, 59); 
+	/* Using write to print to stdout */
 	return (1);
 }
