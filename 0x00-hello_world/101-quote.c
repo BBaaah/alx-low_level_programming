183ea8f809ae1dd853cc073319a1e6951db23430
Bbaaah <Bbaaah>
2023-10-08 23:23:05 -0700

    update

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 #include <unistd.h>
+
 /**
  * main - A c program that prints a line to the standard error
  * The entry point of the program
  * This function writes a specific string to the standard output (stdout)
- * @return Always returns 1 to indicate an error occurred.
+ * Return: Always 1 (Success)
  */
 int main(void)
 {
-	const char *str = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	write(STDOUT_FILENO, str, 59); 
-	/* Using write to print to stdout */
+	write(2,
+	"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";59);
 	return (1);
 }
