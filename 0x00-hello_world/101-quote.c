77fb203f59b7d58c02b14c59c451022c4c66ec9e
Bbaaah <Bbaaah>
2023-10-09 21:18:59 -0700

    update

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,15 +1,11 @@
-#include <stdio.h>
 #include <unistd.h>
-
 /**
- * main - A c program that prints a line to the standard error
- * The entry point of the program
- * This function writes a specific string to the standard output (stdout)
- * Return: Always 1 (Success)
+ * main - the entry point
+ * Return:0 (Success)
  */
+
 int main(void)
 {
-	write(2,
-	"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
+
 /**
- * main - A C program that prints the size of various types on the computer it is compiled and run on
+ * main - A c program that prints the size of various types on the computer it is compiled and run on.
+ *
  * Return 0 (Success)
  */
 int main(void)
@@ -9,12 +11,13 @@ int main(void)
 	int b;
 	long int c;
 	long long int d;
-	float f;
+	float e;
 
 	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
 	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(b));
 	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(c));
 	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(d));
-	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
+	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(e));
 	return (0);
 }
+
