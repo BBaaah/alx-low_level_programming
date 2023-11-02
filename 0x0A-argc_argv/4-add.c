4576500c9dc9917053aff03e0ec616cefd34aeae
Bbaaah <Bbaaah>
2023-11-01 18:29:46 -0700

    update

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,75 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
-#include <string.h>
+
 /**
- * check_num - check if a string containes only digits.
- * @str: The input string to be checked.
+ * main - adds positive numbers
+ * @argc: argument count
+ * @argv: arguments
  *
- * Return: 1 if the string contains only digits, 0 otherwise.
+ * Return: 0 on success, 1 if an arguement is not a number
  */
-int check_num(char *str)
+int main(int argc, char **argv)
 {
-	unsigned int count;
+	int i, n, sum = 0;
+	char *flag;
 
-
-		count = 0;
-		while (count < strlen(str)) /*count string*/
-
-
-		{
-			if (!isdigit(str[count])) /*check if str there are digit*/
-			{
-				return (0);
-			}
-
-
-			count++;
-		}
-		return (1);
-	}
-
-
-	/**
-	 * main - Entry point of the program that sum the valid integers..
-	 * @argc: Count arguments
-	 * @argv: Arguments
-	 *
-	 * Return: Always 0 (Success)
-	 */
-
-int main(int argc, char *argv[])
-{
-	int count;
-	int str_to_int;
-	int sum = 0;
-
-
-		count = 1;
-		while (count < argc) /*Goes through the whole array*/
-		{
-			if (check_num(argv[count]))
-
-
-			{
-				str_to_int = atoi(argv[count]); /*ATOI --> convert string to int*/
-				sum += str_to_int;
-			}
-
-
-			else
-			{
-				printf("Error\n");
-				return (1);
-			}
-
-
-			count++;
-		}
-
-
-		printf("%d\n", sum); /*print sum*/
-
-
+	if (argc < 2)
+	{
+		printf("0\n");
 		return (0);
 	}
+	for (i = 1; i < argc, i++)
+	{
+		n = strol(argv[i], &flag, 10);
+		if (*flag)
+		{
+			printf("Error\n");
+			return (1);
+		}
+		else
+		{
+			sum += n;
+		}
+	}
+printf("%d\n", sum);
+return (0);
+}
