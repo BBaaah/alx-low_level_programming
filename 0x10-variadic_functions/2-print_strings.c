196003714f0e07efde8147add3f77811e44c606b
Bbaaah <Bbaaah>
2023-11-09 05:49:46 -0800

    update

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -11,23 +11,20 @@
 
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
+	char *str;
 	unsigned int i;
 	va_list ap;
 
-
-
 	va_start(ap, n);
-
-
-
 	if (separator == NULL)
 		separator = "";
 
-
-
 	for (i = 0; i < n; i++)
 	{
-		printf("%d", va_arg(ap, int));
+		str = va_arg(ap, char*);
+		if (str == NULL)
+			str = "(nil)";
+		printf("%s", str);
 		if (i < n - 1)
 			printf("%s", separator);
 	}
